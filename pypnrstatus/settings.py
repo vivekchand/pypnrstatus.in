bf3c09cf26a46236b8d8a7f4e8b093266d50675f
Vivek Chand <vivekchand19@gmail.com>
2014-02-01 19:18:34 +0530

    removed dj_database_url import

--- pypnrstatus/settings.py
+++ pypnrstatus/settings.py
@@ -60,15 +60,12 @@ WSGI_APPLICATION = 'pypnrstatus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
 }
-if dj_database_url.config():
-    DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
