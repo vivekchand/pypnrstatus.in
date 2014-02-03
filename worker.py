847365579831e9f4618ecd533db4741603dd6e7c
Vivek Chand <vivekchand19@gmail.com>
2014-02-03 23:16:27 +0530

    decreased time range to 2 min

--- worker.py
+++ worker.py
@@ -24,13 +24,13 @@ import time
 
 while True:
     print 'I am doing something'
-    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=5))
+    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=2))
     print pnr_notifications
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
-    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__gte=datetime.now()-timedelta(minutes=5))
+    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__gte=datetime.now()-timedelta(minutes=2))
     print pnr_notifications
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
 
-    #time.sleep(5*60)
+    time.sleep(1*60)
