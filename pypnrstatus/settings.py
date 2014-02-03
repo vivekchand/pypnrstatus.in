4949bd109c81597dc3fb420072d0d5490d9f8dc8
Vivek Chand <vivekchand19@gmail.com>
2014-02-03 23:49:52 +0530

    dont use postgress

--- pypnrstatus/settings.py
+++ pypnrstatus/settings.py
@@ -63,14 +63,10 @@ WSGI_APPLICATION = 'pypnrstatus.wsgi.application'
 import dj_database_url
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
 }
-'''
 if dj_database_url.config():
     DATABASES['default'] =  dj_database_url.config()
-'''
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
