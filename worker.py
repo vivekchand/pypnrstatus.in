47235b9a04a85d4039ee05f59f5d000b97d652f8
Vivek Chand <vivekchand19@gmail.com>
2014-02-03 22:50:47 +0530

    commented unnecessary code

--- worker.py
+++ worker.py
@@ -1,15 +1,23 @@
+'''
 import os
 from django.conf import settings
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if not settings.configured:
-    settings.configure()
 import dj_database_url
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+}
+
 if dj_database_url.config():
     settings.DATABASES['default'] =  dj_database_url.config()
 
-
+if not settings.configured:
+    settings.configure()
+'''
 from pypnrstatus.tasks import *
 from pypnrstatus.models import *
 from datetime import timedelta, datetime
