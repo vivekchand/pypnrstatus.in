5613d676e5a34ab5e00312018067c7492ad51258
Vivek Chand <vivekchand19@gmail.com>
2014-02-03 22:56:13 +0530

    revert worker.py changes

--- worker.py
+++ worker.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+if not settings.configured:
+    settings.configure()
 import dj_database_url
 DATABASES = {
      'default': {
@@ -14,8 +16,6 @@ DATABASES = {
 if dj_database_url.config():
     settings.DATABASES['default'] =  dj_database_url.config()
 
-if not settings.configured:
-    settings.configure()
 
 from pypnrstatus.tasks import *
 from pypnrstatus.models import *
