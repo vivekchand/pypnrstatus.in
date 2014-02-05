b7882c29830187971c523e9ddacee0755c512c95
Vivek Chand <vivekchand19@gmail.com>
2014-02-05 22:30:28 +0530

    sleep for 1 min if no work

--- worker.py
+++ worker.py
@@ -11,5 +11,11 @@ while True:
     print 'I am doing something'
     pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=1))
     print pnr_notifications
+    begin = time.time()
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
+    end = time.time()
+    diff = int(begin-end)
+    print diff
+    if diff == 0:
+        sleep(1*60)
