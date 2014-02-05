6f07d8b9f5784b49b49725932b9d052bf4d365bd
Vivek Chand <vivekchand19@gmail.com>
2014-02-05 23:00:24 +0530

    increase bracket to 5 min

--- worker.py
+++ worker.py
@@ -9,15 +9,15 @@ import time
 
 while True:
     print 'I am doing something'
-    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=1),
-            next_schedule_time__gte=datetime.now()-timedelta(minutes=1))
+    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=5),
+            next_schedule_time__gte=datetime.now()-timedelta(minutes=5))
     print pnr_notifications
     begin = time.time()
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
     end = time.time()
     diff = int(end-begin)
-    sleep_time = int((1*60) - (diff/60))
+    sleep_time = int((5*60) - (diff/60))
     print 'sleeping for %s min' % (sleep_time/60)
     if sleep_time>0:
         time.sleep(sleep_time)
