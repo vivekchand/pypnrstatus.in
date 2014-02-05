e00cd8b286edd8af8df09a569e9d03782b79d301
Vivek Chand <vivekchand19@gmail.com>
2014-02-05 22:43:14 +0530

    check in bracket of 1 min

--- worker.py
+++ worker.py
@@ -9,10 +9,14 @@ import time
 
 while True:
     print 'I am doing something'
-    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=1))
+    pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=1),
+            next_schedule_time__gte=datetime.now()-timedelta(minutes=1))
     print pnr_notifications
     begin = time.time()
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
+    end = time.time()
+    sleep_time = int(end-begin)
+    print 'sleep time %s' % sleep_time
     print 'sleeping for 1 min'
     time.sleep(1*60)
