f7dcaeb3fdb2f1c834331fda21a7838325272adc
Vivek Chand <vivekchand19@gmail.com>
2014-02-05 22:11:17 +0530

    Caluclate sleep time correctly

--- worker.py
+++ worker.py
@@ -12,7 +12,11 @@ while True:
     pnr_notifications = PNRNotification.objects.filter(next_schedule_time__lte=datetime.now()+timedelta(minutes=5),
         next_schedule_time__gte=datetime.now()-timedelta(minutes=5))
     print pnr_notifications
+    start = time.time()
     for pnr_notification in pnr_notifications:
         schedule_pnr_notification(pnr_notification)
-
-    time.sleep(5*60)
+    end = time.time()
+    diff = int(end-start)
+    sleep_time = (5*60)-(diff/60)
+    if sleep_time>0:
+        time.sleep(sleep_time)
